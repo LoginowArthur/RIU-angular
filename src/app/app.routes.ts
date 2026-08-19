6b3234dcd7d9839e698b641575bc6b7f18e2af9e
Arthur Loginow <loginowarthur@gmail.com>
2026-08-19 20:18:14 -0300

    feat: basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+// import { HomeComponent } from ''
+import { App } from './app';
+import { HeroForm } from './components/hero-form/hero-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: App },
+  { path: 'heroes/add', component: HeroForm },
+  { path: 'heroes/edit/:id', component: HeroForm },
+  { path: '**', redirectTo: 'home' }
+];
