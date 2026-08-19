aaebb5b7388648afc673eb8145b78c36c3687219
Arthur Loginow <loginowarthur@gmail.com>
2026-08-19 19:01:50 -0300

    feat(hero-form): add material UI to form and save button

--- src/app/components/hero-form/hero-form.ts
+++ src/app/components/hero-form/hero-form.ts
@@ -1,18 +1,44 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-hero-form',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule
+  ],
   templateUrl: './hero-form.html',
   styleUrl: './hero-form.scss',
 })
 export class HeroForm {
   heroForm = new FormGroup({
-    name: new FormControl(''),
-    location: new FormControl(''),
-    power: new FormControl(0),
-    status: new FormControl('available'),
-    motto: new FormControl('')
+    name: new FormControl('', [
+      Validators.required,
+    ]),
+    location: new FormControl('', [
+      Validators.required,
+    ]),
+    power: new FormControl(0, [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(100)
+    ]),
+    status: new FormControl('1'),
+    motto: new FormControl('', [
+      Validators.required,
+    ])
   });
+
+  onSubmit(): void {
+    if (this.heroForm.valid) {
+      console.log('form is valid');
+    }
+  }
 }
