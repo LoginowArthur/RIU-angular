61c45249e1e039635044eb42087539751cc1df14
Arthur Loginow <loginowarthur@gmail.com>
2026-08-21 10:26:14 -0300

    feat(routes): implement lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
-import { Home } from './features/home/home';
-import { HeroForm } from './features/heroes/components/hero-form/hero-form';
-import { HeroFormDialogContainer } from './features/heroes/containers/hero-form-dialog-container/hero-form-dialog-container'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
-  { path: 'heroes/add', component: HeroFormDialogContainer },
-  { path: 'heroes/edit/:id', component: HeroFormDialogContainer },
-  { path: '**', redirectTo: 'home' }
+  { 
+    path: 'home', 
+    loadComponent: () => import('./features/home/home').then(m => m.Home)
+  },
+  { 
+    path: 'heroes/add', 
+    loadComponent: () => import('./features/heroes/containers/hero-form-dialog-container/hero-form-dialog-container').then(m => m.HeroFormDialogContainer)
+  },
+  { 
+    path: 'heroes/edit/:id', 
+    loadComponent: () => import('./features/heroes/containers/hero-form-dialog-container/hero-form-dialog-container').then(m => m.HeroFormDialogContainer)
+  },
+  { 
+    path: '**', 
+    redirectTo: 'home' 
+  }
 ];
