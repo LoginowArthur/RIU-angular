4506e26a5f988c477701e7e6489334ef970d5763
Arthur Loginow <loginowarthur@gmail.com>
2026-08-24 10:09:48 -0300

    feat: implement add hero on list

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,27 +1,56 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Hero } from '../heroes/models/hero.model'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { HeroesListItem } from './components/heroes-list-item/heroes-list-item'
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { HeroesListItem } from './components/heroes-list-item/heroes-list-item';
 import { Heroes } from '../heroes/services/heroes';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroesListItem, AsyncPipe, MatFormFieldModule, MatInputModule],
+  imports: [
+    HeroesListItem, 
+    AsyncPipe, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatPaginatorModule,
+    MatButtonModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  heroesService = inject(Heroes);
-  heroes = this.heroesService.heroes;
+  private readonly heroesService = inject(Heroes);
+  private readonly router = inject(Router);
+  
+  readonly heroes = this.heroesService.heroes;
+  
+  readonly pageSize = signal<number>(10);
+  readonly pageIndex = signal<number>(0);
 
-  onEdit(id: string) {
+
+  readonly paginatedHeroes = computed(() => {
+    const start = this.pageIndex() * this.pageSize();
+    const end = start + this.pageSize();
+    return this.heroes().slice(start, end);
+  });
+
+  handlePageEvent(event: PageEvent): void {
+    this.pageSize.set(event.pageSize);
+    this.pageIndex.set(event.pageIndex);
+  }
+
+  onAdd(): void {
+    this.router.navigate(['/heroes/add']);
+  }
+
+  onEdit(id: string): void {
     console.log('on edit', id);
   }
 
-  onDelete(id: string) {
+  onDelete(id: string): void {
     this.heroesService.removeHero(id);
   }
 }
