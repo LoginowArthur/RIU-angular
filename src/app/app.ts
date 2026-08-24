be9e91af2ef18286a933a476f013dc4512df95c3
Arthur Loginow <loginowarthur@gmail.com>
2026-08-24 09:21:26 -0300

    feat: get heroes in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Spinner } from './features/heroes/services/spinner'
+import { Heroes } from './features/heroes/services/heroes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatProgressSpinnerModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  
+export class App implements OnInit {
+  heroesService = inject(Heroes);
+
+  ngOnInit() {
+    this.heroesService.loadHeroes();
+  }
 }
